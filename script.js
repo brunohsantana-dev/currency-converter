564d257f173626923baed389804b32a4fc2e0296
brunohsantana-dev <brunohsantana.dev@gmail.com>
2026-09-10 16:20:08 -0300

    following devclub task til js pt 3

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
 const convertButton = document.querySelector(".convert-button");
+const currencyFromSelect = document.querySelector(".currency-from");
+const currencyToSelect = document.querySelector(".currency-to");
 
 function convertCurrency() {
-    const inputAmoutValue = document.querySelector(".input-amount").value;
+    const inputAmountValue = document.querySelector(".input-amount").value;
+    const currencyAmountToConvert = document.querySelector(".amount-to-convert")
+    const currencyAmountConverted = document.querySelector(".amount-converted")
 
-    const currencyAmountToConvert = document.querySelector(".amount-to-convert");
-    const currencyAmountConverted = document.querySelector(".amount-converted");
+    console.log(currencyFromSelect.value)
+    console.log(currencyToSelect.value)
 
-    currencyAmountToConvert.innerHTML = inputAmoutValue
-    currencyAmountConverted.innerHTML = convertedValue
+    const dolarToday = 5.50
+    const euroToday = 6.50
+    const libraToday = 7.50
+    const audToday = 3.50
 
-    const dolarToday = 5.50;
-    const euroToday = 6.50;
-    const libraToday = 7.50;
-    const audToday = 3.50;
+    const AmountConverted = inputAmountValue / dolarToday;
 
-    const dolarValue = (inputAmoutValue / dolarToday);
-    const euroValue = (inputAmoutValue / euroToday);
-    const libraValue = (inputAmoutValue / libraToday);
-    const audValue = (inputAmoutValue / audToday);
+    currencyAmountToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }).format(inputAmountValue)
 
-    console.log(inputAmoutValue);
-    console.log(dolarValue);
-    console.log(euroValue);
-    console.log(libraValue);
-    console.log(audValue);
+    currencyAmountConverted.innerHTML = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD"
+    }).format(AmountConverted)
+
 }
 convertButton.addEventListener("click", convertCurrency)
